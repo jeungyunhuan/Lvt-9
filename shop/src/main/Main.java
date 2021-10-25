89d1c8ecb365bc472c13ffe8d22cf012b4ea39ab
dbsgh <dbsgh@DESKTOP-6GKMBB6>
2021-10-25 18:29:47 +0900

    zombie

---
+++ shop/src/main/Main.java
@@ -0,0 +1,9 @@
+package main;
+
+import controller.Game;
+
+public class Main {
+	public static void main(String[] args) {
+		Game.getinstance().run();
+	}
+}
---
+++ shop/src/model/Zombie.java
@@ -0,0 +1,11 @@
+package model;
+
+import controller.Unit;
+
+public class Zombie extends Unit{
+	public Zombie(String name,int hp,int att,int def,int pos) {
+		super(name, hp, att, def,pos);
+		// TODO Auto-generated constructor stub
+	}
+
+}
